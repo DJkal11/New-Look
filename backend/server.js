ce41d6e1fa9ee502259c4f8a07072e1a72dab134
DJkal11 <73737598+DJkal11@users.noreply.github.com>
2021-04-18 17:36:08 +0200

    make it better

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const exithook = require('exit-hook');
+const path = require('path');
 
 require('dotenv').config();
 
@@ -10,6 +11,7 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
 
 const uri = process.env.ATLAS_URI;
 mongoose.connect(uri, {useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true});
@@ -27,6 +29,10 @@ app.use('/users', usersRouter);
 if (process.env.NODE_ENV === 'production')
 {
     app.use(express.static('build'));
+
+    app.get('*', (req, res) => {
+res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+    });
 }
 
 app.listen(port, () => {
