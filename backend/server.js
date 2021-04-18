1b2c9930eac7f351a84a506325d381635f7db389
DJkal11 <73737598+DJkal11@users.noreply.github.com>
2021-04-18 18:48:45 +0200

    up

--- backend/server.js
+++ backend/server.js
@@ -30,9 +30,6 @@ if (process.env.NODE_ENV === 'production')
 {
     app.use(express.static('build'));
 
-    app.get('*', (req, res) => {
-res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
-    });
 }
 
 app.listen(port, () => {
